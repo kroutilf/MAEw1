adeffa4cb21b07263e0122d319770098d4cd4f70
kroutilf <72045867@users.noreply.github.com>
2021-10-04 22:54:10 +0200

    Homework: fruit_size

--- Script w1.R
+++ Script w1.R
@@ -1,3 +1,5 @@
+fruit_size <- c(2, 4, 7, 5, 3, 2)
+
 length1 <- function(ve1=c(1,1))
 {
   if(sum(ve1)==0)
@@ -6,10 +8,11 @@ length1 <- function(ve1=c(1,1))
   {sum(abs(ve1))/mean(abs(ve1))}
 }
 length1(c(0,0,0))
+length1(fruit_size)
 
 length2 <- function(ve1=c(1,1),p=0)
 {
   sum(as.logical(ve1+p))
 }
 length2(c(-1,6,0),2)#use p if 0 present
-
+length2(fruit_size)
