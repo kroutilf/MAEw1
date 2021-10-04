cfe5245bde702e311e4335b957f41acc0245785d
kroutilf <72045867@users.noreply.github.com>
2021-10-04 22:49:15 +0200

    Homework: Vector length

--- Script w1.R
+++ Script w1.R
@@ -1,4 +1,15 @@
-x<-1
-y<-5
-y=3
+length1 <- function(ve1=c(1,1))
+{
+  if(sum(ve1)==0)
+  {sum(abs(ve1+1))/mean(abs(ve1+1))}
+  else
+  {sum(abs(ve1))/mean(abs(ve1))}
+}
+length1(c(0,0,0))
+
+length2 <- function(ve1=c(1,1),p=0)
+{
+  sum(as.logical(ve1+p))
+}
+length2(c(-1,6,0),2)#use p if 0 present
 
